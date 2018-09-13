648be899872e6b9dd7bf88d297e1e612ad0ffb17
gmaghari <41925578+gmaghari@users.noreply.github.com>
2018-09-12 22:46:31 -0700

    Final Commit

---
+++ Word-Guess-Game/logic.js
@@ -0,0 +1,91 @@
+// VARIABLES
+
+// Array of Zoo Animals
+var wordsList = ["lion", "elephant", "bear", "gorilla", "hippo", "rhino", "tiger"];
+
+var chosenWord = "";
+var animalLetters = [];
+var numBlanks = 0;
+var mixedLetters = [];
+var wrongGuesses = [];
+  
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 10;
+
+
+function startGame() {
+    numGuesses = 10;
+    chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
+    animalLetters = chosenWord.split("");
+    numBlanks = animalLetters.length;
+
+    console.log(chosenWord);
+
+    mixedLetters = [];
+    wrongGuesses = [];
+
+    for (var i = 0; i < numBlanks; i++) {
+        mixedLetters.push("_");
+    }
+  
+    console.log(mixedLetters);
+
+    document.getElementById("guesses-left").innerHTML = numGuesses;
+    document.getElementById("word-blanks").innerHTML = mixedLetters.join(" ");
+    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+}
+  
+
+function checkLetters(letter) {
+    var letterInWord = false;
+    for (var i = 0; i < numBlanks; i++) {
+        if (chosenWord[i] === letter) {
+        letterInWord = true;
+        }
+    }
+
+    if (letterInWord) {
+        for (i = 0; i < numBlanks; i++) {
+            if (chosenWord[i] === letter) {
+                mixedLetters[i] = letter;
+            }
+        }
+    console.log(mixedLetters);
+    }
+    else {
+      wrongGuesses.push(letter);
+      numGuesses--;
+    }
+}
+
+function roundComplete() {
+    console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
+    document.getElementById("guesses-left").innerHTML = numGuesses;
+    document.getElementById("word-blanks").innerHTML = mixedLetters.join(" ");
+    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+  
+    if (animalLetters.toString() === mixedLetters.toString()) {
+        winCounter++;
+        alert("You win!");
+
+        document.getElementById("win-counter").innerHTML = winCounter;
+        startGame();
+    }
+  
+    else if (numGuesses === 0) {
+        lossCounter++;
+        alert("You lose");
+
+        document.getElementById("loss-counter").innerHTML = lossCounter;
+        startGame();
+    }
+}
+
+    startGame();
+    document.onkeyup = function(event) {
+
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    roundComplete();
+};
